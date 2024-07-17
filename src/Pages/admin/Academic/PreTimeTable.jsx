3e2385f992afba23ae5240f61b421471db7b65ca
Himani1805 <hinusharma18@gmail.com>
2024-07-17 19:41:02 +0530

    Redirect branch created

--- src/Pages/admin/Academic/PreTimeTable.jsx
+++ src/Pages/admin/Academic/PreTimeTable.jsx
@@ -77,6 +77,7 @@ export default function PreTimeTable() {
   return (
 
     <Flex >
+      
       <Flex justifyContent={'space-around'} flexWrap={'wrap'} gap={'20px'} alignItems={'center'}>
         <Box>
           <Text>Institution</Text>
@@ -126,25 +127,25 @@ export default function PreTimeTable() {
 
   )
 }
-{/* <Flex justifyContent={'space-around'} gap={"5px"} width={'100%'}>
-        <Box width={'70%'}>
-          <Image src='https://cdn3.iconfinder.com/data/icons/tiny-iconz-vol-07/20/time_clock_history_waiting_timer-64.png' width={"15px"}/>
-          <Text>Prepare Time Table</Text>
-        </Box>
-        <Box width={'80%'} marginLeft={'10px'}>
-          <Button>
-            <Image src='https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_keyboard_arrow_left_48px-64.png' width={"20px"}/>
-            <Text>Back</Text>
-          </Button>
-          <Button backgroundColor={'pink'}>
-            <Image src='https://cdn3.iconfinder.com/data/icons/user-interface-ui-navigation-1/62/Plus-64.png' width={"20px"} />
-            <Text>Setup - College Structure</Text>
-          </Button>
-        </Box>
-      </Flex>
-      <Flex>
-        <Text>Instittution</Text>
-        {/* <ComboBox/> */}
-      //   <DataStore/>
+//  <Flex justifyContent={'space-around'} gap={"5px"} width={'100%'}>
+//         <Box width={'70%'}>
+//           <Image src='https://cdn3.iconfinder.com/data/icons/tiny-iconz-vol-07/20/time_clock_history_waiting_timer-64.png' width={"15px"}/>
+//           <Text>Prepare Time Table</Text>
+//         </Box>
+//         <Box width={'80%'} marginLeft={'10px'}>
+//           <Button>
+//             <Image src='https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_keyboard_arrow_left_48px-64.png' width={"20px"}/>
+//             <Text>Back</Text>
+//           </Button>
+//           <Button backgroundColor={'pink'}>
+//             <Image src='https://cdn3.iconfinder.com/data/icons/user-interface-ui-navigation-1/62/Plus-64.png' width={"20px"} />
+//             <Text>Setup - College Structure</Text>
+//           </Button>
+//         </Box>
+// </Flex>
+      // <Flex>
+      //   <Text>Instittution</Text>
+      //   {/* <ComboBox/> */}
+      // //   <DataStore/>
         
-      // </Flex> */}
+      // // </Flex> */}
